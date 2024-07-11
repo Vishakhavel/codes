475ca59683f0b28397fe098972ddf750396aa8b7
vichu <vichu@vishakhavels-MacBook-Air.local>
2024-07-10 21:11:36 -0700

    finished library management system OOPS

--- OOPS/LibraryManagementSystem/Book.ts
+++ OOPS/LibraryManagementSystem/Book.ts
@@ -1,5 +1,13 @@
 export class Book {
-  title:string = '';
-  author = '';
-  ISBN=
+  title: string = '';
+  author: string = '';
+  ISBN: string | number = '';
+  copies: number = 1;
+
+  constructor(title = '', author = '', ISBN = '', copies = 1) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.copies = copies;
+  }
 }
